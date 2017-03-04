97cb38bf8417ccd29bca334016625a8b37f8289b
YosephCode <yosephcsantos@gmail.com>
2017-03-03 23:02:38 -0300

    withut node_modules

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// import notify from './notification';
+// 
+// require('./main.css');
+// 
+// console.log('notify == ', notify);
+// notify.log("----------------- Webpack works FINE!!");
+// notify.alertMsg("Webpack works FINE!!");
+
+class Person {
+	constructor() {
+			console.log("HEY, I'm works");
+	}
+}
+
+new Person();
---
+++ src/notification.js
@@ -0,0 +1,12 @@
+function log(msg) {
+	console.log(msg);
+}
+
+function alertMsg(msg) {
+	alert(msg);
+}
+
+export default {
+	log : log,
+	alertMsg: alertMsg
+}
